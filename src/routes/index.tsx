84f442dc108d9b1d5e53f9f1db9e579ff8959b38
joko26 <playerone99@outlook.de>
2026-07-30 13:54:44 +0200

    :construction: worked on projects section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,17 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useRef, useState } from "react";
 import { TiledSection } from "#/components/tiledSection";
 import { NineSliceFrame } from "@nine-slice-frame/react";
+import {} from "#/components/tiledSection";
+import ImageFrame from "#/components/imageFrame";
+
+import projectsData from "#/json/projects.json";
+import { getRandomInt } from "#/utils";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   const [age, setAge] = useState("");
+  const projects = Object.entries(projectsData?.projects);
 
   const scrollToSection = (sectionIn: string) => {
     const section = document.getElementById(sectionIn);
@@ -120,11 +126,43 @@ function Home() {
             repeat="repeat"
             fill
             pixelated
-            className="h-full w-full"
-          ></NineSliceFrame>
-          <div>
-            {}
-          </div>
+            className="h-fit w-full"
+          >
+            {" "}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-5 gap-5 items-center justify-center">
+              {projects.map((project, id) => {
+                const frameNumber = getRandomInt(1, 3)
+
+                return (
+                  <NineSliceFrame
+                    imagePath="/box1.png"
+                    slice={12}
+                    borderWidth={40}
+                    repeat="repeat"
+                    fill
+                    pixelated
+                    className="h-full w-full flex items-center justify-center flex-col p-4 space-y-10 "
+                    key={id}
+                  >
+                    <div className="">
+                      <ImageFrame src={project?.[1].img} frameSrc={`/frame${frameNumber}.png`} ></ImageFrame>
+                      <div>
+                        <h1 className="text-3xl" style={{ color: "#a494be" }}>{project?.[1].name}</h1>
+                        <p className="text-xl">{project?.[1].desc}</p>
+                      </div>
+                      <div className="flex flex-row items-center justify-center space-x-5">
+                        {Object.entries(project[1]?.links).map((link, id) => {
+                          return (
+                            <a key={id} className="border-2 rounded-md" href={link?.[1]}>{link?.[0]}</a>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </NineSliceFrame>
+                );
+              })}
+            </div>
+          </NineSliceFrame>
         </TiledSection>
       </section>
       <section id="about">
